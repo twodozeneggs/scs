548060d84f20f8b007c87378b5eb731e6f1f7bb2
twodozeneggs <simonjohnloik@gmail.com>
2023-04-17 15:24:20 -0500

    secondary commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import './components/UnderConstruction';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="assets/southern.png" className="App-logo" alt="logo" />
+        <p className='title'>
+         Southern Coil Solutions is underway!
+        </p>
+        <p2 className='bio'>
+        Get excited, technology driven, specializing in Automotive, Food, Beverage and Battery storage is coming to Bowling Green, Kentucky!
+        </p2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UnderConstruction.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function UnderConstruction() {
+return(
+    <div>
+        <h1>Under Construction</h1>
+        <p>Get excited, cutting edge Steel Storage is coming to Bowling Green, Kentucky!</p>
+    </div>
+);
+}
